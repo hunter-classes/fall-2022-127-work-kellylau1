b7845bbdcd85188df6ed466df935866642b4a97e
Kelly Lau <66286455+kellylau1@users.noreply.github.com>
2022-09-06 16:22:03 +0000

    I completed the chapter_4 homework.

--- chapter_4/seven.py
+++ chapter_4/seven.py
@@ -1,1 +1,27 @@
-# Write a program to draw a face of a clock that looks something like this:
+import turtle
+
+wn = turtle.Screen()
+greener = turtle.Turtle()
+wn.bgcolor("light green")
+greener.color("blue")
+greener.shape("turtle")
+greener.width(2)
+greener.stamp()
+
+
+greener.right(30)
+greener.penup()
+
+for i in range(11):
+    greener.left(30)
+    greener.goto(0,0)
+    greener.forward(120)
+    greener.pendown()
+    greener.forward(10)
+    greener.penup()
+    greener.forward(20)
+    greener.stamp()
+
+
+wn.exitonclick()
+wn.mainloop()
