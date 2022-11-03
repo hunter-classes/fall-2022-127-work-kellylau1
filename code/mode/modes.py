abcc6f1b603cf179a13050aabfe9cb7f88983d3b
Kelly Lau <66286455+kellylau1@users.noreply.github.com>
2022-11-03 02:26:42 +0000

    I completed the project-01-madlibs midterm.

---
+++ code/mode/modes.py
@@ -0,0 +1,149 @@
+# # findLargest(l) which takes in a list of numbers and returns the value of the smallest number
+
+# def findLargest(l):
+#     l.sort()
+#     return l[:1]
+
+# l = [3, 1, 2]
+# print(findLargest(l))
+
+# # freq(l,v) which takes a list of numbers (l) and a value (v). The function will return the freuqeency of v, that is, the number of times that v appears in l.
+
+# def freq(l, v):
+
+#     for i in l:
+#         if i < v or i > v:
+#             l.remove(i)
+#     return len(l)
+
+# l = [5, 5, 2, 5, 1, 5]
+# print(freq(l, 5))
+
+# Returns a mode of the dataset, that is the value that appears most frequently if multiple values appear the same # of times and are most frequent, return any of them.
+# Ex: mode([5,4,5,6,7,8,5,4]) --> 5 since 5 appears the most mode([5,5,5,4,4,4,2,2,7,7,8,8,9]) --> return 5 or 4 since both of those values appear 3 times which is the most
+# Strategy: assume the first value is the mode we can grab its frequency for each remaining item in the dataset:count that items frequence and see if it's the new mode so far
+
+import datetime
+import random
+
+
+# def mode(dataset):
+#     modeSoFar = dataset[0]
+#     freqSoFar = freq(dataset, modeSoFar)
+#     for item in dataset[1:]:
+#         if freq(dataset, item) > freqSoFar:
+#             modeSoFar = item
+#             freqSoFar = freq(dataset, item)
+
+#     return modeSoFar
+
+
+# def testMode(size, maxValue):
+#     dataset = buildRandomList(size, maxValue)
+#     # print(dataset)
+#     t = datetime.datetime.now()
+#     starttime = t.microsecond / 1000
+#     m = mode(dataset)
+#     end = datetime.datetime.now()
+#     elapsed = (end.microsecond / 1000) - starttime
+#     print("size: ", size, " time: ", elapsed)
+
+  
+
+# def findLargest(dataset):
+#     largeSoFar = dataset[0]
+#     for item in dataset[1:]:
+#         if item > largeSoFar:
+#             largeSoFar = item
+#     return largeSoFar
+
+
+
+# def freq(dataset,v):
+#     # since this loops over the entire data set once it takes n time 
+#     #count = 0
+#     #for item in dataset:
+#     #    if item == v:
+#     #        count = count + 1
+#     #return count
+#     return len([x for x in dataset if x == v])
+
+# def buildRandomList(size,maxvalue):
+#     #result = []
+#     #for x in range(size):
+#     #    result.append(random.randrange(maxvalue))
+#     #return result
+#     result = [random.randrange(maxvalue) for x in range(size)]
+#     return result 
+
+
+# def mode(dataset):
+#     """
+#     Returns a mode of the dataset, that is
+#     the value that appears most frequently
+#     if multiple values appear the same # of times and are
+#     most frequent, return any of them.
+#     Ex: mode([5,4,5,6,7,8,5,4]) --> 5 since 5 appears the most
+#     mode([5,5,5,4,4,4,2,2,7,7,8,8,9]) --> return 5 or 4 since
+#     both of those values appear 3 times which is the most
+#     Strategy:
+#     assume the first value is the mode
+#     we can grab its frequency
+#     for each remaining item in the dataset:
+#       count that items frequence and see if it's the new
+#       mode so far    
+#     """
+#     modeSoFar = dataset[0]
+#     freqSoFar = dataset.count(modeSoFar)
+#     for item in dataset[1:]: #outer loop -> n
+#         # calling freq each time is n
+#         # if freq(dataset,item) > freqSoFar:
+#         if dataset.count(item) > freqSoFar:
+#             modeSoFar = item
+#             freqSoFar = dataset.count(item)
+#     return modeSoFar
+
+
+def fastMode(dataset):
+    randomlList(2,3,9,3,4)
+    tallies[0,0,1,2,1,0,0,0,0,1]
+
+
+    # assume all values in dataset
+    # are between 0 and 99 inclusive
+
+    # 1. make a list of 100 slots
+    # and set them all to 0
+    # this will store our tallies
+
+    # 2. Loop through our dataset
+    # and for each item incremement
+    # (add 1) to the appropriate
+    # slot in the tallies list
+
+    # 3. the index with the highest
+    # value in tallies is the mode
+
+
+
+
+  
+    # pass
+
+    
+# def testMode(size,maxValue):
+#     print("Dataset Size: ",size)
+#     dataset = buildRandomList(size,maxValue)
+#     # print(dataset)
+#     m = mode(dataset)
+#     print("Mode: ",m)
+
+# def testFindLargest(size,maxValue):
+#     print("Dataset Size: ",size)
+#     dataset = buildRandomList(size,maxValue)
+#     # print(dataset)
+#     m = findLargest(dataset)
+#     print("Largest: ",m)
+
+# #testFindLargest(80000,30)
+# testMode(40000,30)
--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -1,32 +1,78 @@
+# Extras:
+# - Add some replacements that are unique, that is, the first time you see them you select on randomly but then you keep reusing that replacement.
+# - Instead of specifying the sentences or story to convert, write a story in a file and read it from your program. Make sure to include the file in your repo and that your program reads it correctly. 
+
+open_file = open("story.txt" , "r")
+print(open_file.read())
+open_file.close()
+
 import random 
 
-string = "Sam <verb> the <noun> and then <verb> the <noun> later."
+string = "Kelly <verb> around the <noun> yesterday because she felt sick. Then in the morning, she <verb> with the <noun> for five minutes."
+
+string1 = "Rachel <verb> with the <adj> <noun> in the morning and then <verb> with the <adj> <noun> in the afternoon."
+
+string2 = "<hero> <verb> with <pronoun> <noun> and then <hero> <verb> with <pronoun> <noun> later."
 
 verbs = ['ate','walked','slept']
 nouns = ['dog','hammer','cat','car','frog']
+adjs = ['delicious', 'fast', 'jealous']
+heros = ['Superman', 'WonderWoman', 'Batman']
+pronouns = ['her', 'his']
 
 words = string.split()
+words1 = string1.split()
+words2 = string2.split()
 
 def substitute(words):
-  for verb in words: 
-    verb == "<verb>"
-    random_verb = random.choice(verbs)
-    verb = verb.replace(verb, random_verb)
-  print(str(verb))
-  for noun in words: 
-    noun == "<noun>"
-    random_noun = random.choice(nouns)
-    noun = noun.replace(noun, random_noun)
-  print(str(noun))
-
-print(words)
-
-
   
+  for i in range(len(words)):
+    if words[i] == '<verb>':
+      words[i] = str(random.choice(verbs))
+
+    if words[i] == '<noun>':
+      words[i] = str(random.choice(nouns))    
+        
+  print(" ".join(words))
+substitute(words)
+
+def substitute1(words1):
+
+  for i in range(len(words1)): 
+
+    if words1[i] == '<verb>':
+      words1[i] = str(random.choice(verbs))
+       
+    if words1[i] == '<adj>':
+      words1[i] = str(random.choice(adjs))
+
+    if words1[i] == '<noun>':
+      words1[i] = str(random.choice(nouns))
+  
+  print(" ".join(words1))
+substitute1(words1)
 
 
+def substitute2(words2):
 
-      
+  for i in range(len(words2)): 
+    if words2[i] == '<noun>':
+      words2[i] = str(random.choice(nouns))
     
+    if words2[i] == '<verb>':
+      words2[i] = str(random.choice(verbs))
+      
+    if words2[i] == '<hero>':
+      random.seed(1)
+      choose_hero = str(random.choice(heros))
+      words2[i] = choose_hero
+      
+    if words2[i] == '<pronoun>':
+      random.seed(1)
+      choose_pronoun = str(random.choice(pronouns))
+      words2[i] = choose_pronoun    
+      
+  print(" ".join(words2))
+substitute2(words2)
 
-
+ 
